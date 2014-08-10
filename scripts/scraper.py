b2d442f70af349287a7565872e9810f7da56f70a
Edouard <edou4rd@gmail.com>
2014-08-10 20:45:02 +0200

    molecules

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -16,6 +16,45 @@ from lxml import etree, html
 import json
 import requests
 import os
+import sys
+
+MOLECULES = map(str.lower, [
+    "TFMPP",
+    "MDMA*HCl",
+    "MDMA*HCI",
+    "Methamphetamin",
+    "Amphetamin*HCl",
+    "MBZP",
+    "mdma",
+    "mda",
+    "Paracetamol",
+    "Methadon",
+    "pmma",
+    "2C-B",
+    "25C-NBOMe",
+    "25I-NBOMe",
+    "MDHOET",
+    "Amphetamin",
+    "Fluorampheatmin",
+    "mcpp",
+    "m-cpp",
+    "methylon",
+    "F-FA",
+    "mephedron",
+    "fluoramphetamin",
+    "Koffein",
+    "Domperidon"
+    "MDPV",
+    "Amphetamin",
+    "Amphetami",
+    "Coffein",
+    "MDDMA",
+    "4-Fa",
+    "Buflomedil",
+    "Amoxicillin",
+    "m-CCP",
+    "4-F-A",
+    ])
 
 if __name__ == "__main__":
     results = []
@@ -40,7 +79,61 @@ if __name__ == "__main__":
             if tag_idx > -1:
                 key, value = line[tag_idx+8:].replace("</strong>", "").split(":", 1)
                 infos[key.lower()] = value.rstrip().lstrip()
+        # composition
+        inhalt =  infos.get("inhalt")
         results.append(infos)
+    import re
+    import itertools
+    for i, result in enumerate(results):
+        line = result.get("inhalt", "")
+        mdma = ""
+        if "mdma" in line.lower():
+            number = ""
+            mol    = ""
+            is_a_digit = False
+            is_a_mol   = False
+            for _ in line:
+                if _.isdigit() or _ in [".", ","]:
+                    is_a_digit = True
+        composition = {}
+        mol_is_before_number = False
+        every_char_exept_unit = "-qwertyuiopasdfhjklzxcvbnm"
+        number_occurences, number_occurences_bck = itertools.tee(re.finditer("(\d+(?!["+every_char_exept_unit+"])\.?,? ?\d*\ ?(?!["+every_char_exept_unit+"])*(mg|g)?)(?!-)", line, re.IGNORECASE))
+        number_occurences = list(number_occurences)
+        try:
+            before_first_number = line[:number_occurences_bck.next().start()]
+            if before_first_number:
+                if not "+" in before_first_number:
+                    mol_is_before_number = True
+            if mol_is_before_number:
+                # search before
+                previous_occ = 0
+                for occ in number_occurences:
+                    before = line[previous_occ:occ.start()]
+                    for mol in MOLECULES:
+                        if mol in before.lower():
+                            composition[mol] = occ.group(0)
+                            break
+                    previous_occ = occ.end()
+            else:
+                number_occurences.reverse()
+                if not number_occurences: continue
+                next_occ = None
+                for occ in number_occurences:
+                    after = line[occ.end():next_occ]
+                    for mol in MOLECULES:
+                        if mol in after.lower():
+                            composition[mol] = occ.group(0)
+                            break
+                    next_occ = 0 - (len(line) - occ.start())
+                pass
+        except StopIteration as e:
+            pass
+        # if len(composition) != len(number_occurences):
+        #     print line
+        #     print [_.group(0) for _ in number_occurences], composition
+        result["composition"] = composition
+    # print sum(len(_["composition"])> 0 for _ in results), "/", len(results)
     print json.dumps(results, indent=4)
 
 # EOF
